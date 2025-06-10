ff673d41bcc77b40e1d6226ca6358ef25b11828a
ayushKhandelwal07 <ayushkhandelwal227@gmail.com>
2025-06-10 16:56:07 +0530

    test

--- client/src/game/backend.ts
+++ client/src/game/backend.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = import.meta.env.DEV
     ? "ws://localhost:2567"
-    : import.meta.env.VITE_BACKEND_URL;
+    : import.meta.env.VITE_BACKEND_URL || "wss://metaverse.ayushworks.tech";
