c717c0155c3025ba3dc9575fcdec4ba91be7576f
ayushKhandelwal07 <ayushkhandelwal227@gmail.com>
2025-06-10 19:02:30 +0530

    fix/two_be

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -8,13 +8,13 @@ const os = require('os');
 
 module.exports = {
   apps : [{
-    name: "colyseus-app",
+    name: "metaverse-backend",
     script: 'build/index.js',
     time: true,
     watch: false,
-    instances: os.cpus().length,
+    instances: 1,
     exec_mode: 'fork',
-    wait_ready: true,
+    wait_ready: false,
     env_production: {
       NODE_ENV: 'production'
     }
